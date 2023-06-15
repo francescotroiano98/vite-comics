1f003563fe3b1102e5e0c24767610950898a2c98
francescotroiano98 <francesco-troiano-98@libero.it>
2023-06-15 17:17:38 +0200

    trying create footer  link list

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ export default {
 
   <AppHeader/>
   <AppMain/>
+  <AppFooter/>
 
 </template>
 
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,111 @@
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <footer>
+        <section class="footer-top">
+            <div class="container">
+
+                <div class="left-nav-content">
+                    <nav>
+                        <ul>
+                            <li v-for="link in footerLink">{{ link.name }} {{ link.link }}</li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="right-content">
+                    <img src="../assets/img/dc-logo-bg.png" alt="">
+
+                </div>
+
+
+            </div>
+            
+        </section>
+        <section class="footer-bottom">
+
+        </section>
+    </footer>
 </template>
 <script>
 export default {
+    data(){
+        return{
+            footerLink:[{
+                name: 'DC COMICS',
+                link:[
+                'CHARACTERS',
+                'COMICS',
+                'MOVIES',
+                'TV',
+                'GAMES',
+                'COLLECTIBLES',
+                'VIDEOS',
+                'FANS',
+                'NEWS'
+                ]
+            },
+            {
+                name: 'DC',
+                link:[
+                "CHARACTERS",
+                'COMICS',
+                'MOVIES',
+                'TV',
+                'GAMES',
+                'COLLECTIBLES',
+                'VIDEOS',
+                'FANS',
+                'NEWS'
+                ]
+            },
+            {
+                name: 'SITES',
+                link:[
+                'CHARACTERS',
+                'COMICS',
+                'FANS',
+                'NEWS'
+                ]
+            },
+            {
+                name: 'SHOP',
+                link:[
+                
+                'COMICS',
+                'MOVIES',
+                'GAMES',
+                'COLLECTIBLES',
+                
+                ]
+            },
+                
+            ]
+        }
+    }
     
 }
 </script>
 <style lang="scss">
+.footer-top{
+    background-image: url(../assets/img/footer-bg.jpg);
+    background-size: cover;
+    
+}
+.left-nav-content{
+    width: 50%;
+}
+.right-content{
+display: flex;
+align-items: center;
+justify-content: center;
+width: 50%;
+}
+.right-content img{
+
+width: 50%;
+
+}
+li{
+    color: white;
+}
     
 </style>
